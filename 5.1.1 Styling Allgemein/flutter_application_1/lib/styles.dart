e9f3569a9c450e0b4a07bb611ba6f41fdd6a4c5d
pavlozelenov@gmail.com <pavlozelenov@gmail.com>
2023-10-31 14:46:45 +0100

    Styles.dart

---
+++ 5.1.1 Styling Allgemein/flutter_application_1/lib/styles.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+TextStyle headerText = const TextStyle(
+    fontSize: 18.0, fontWeight: FontWeight.bold, color: Colors.amber);
+
+TextStyle bodyText = const TextStyle(fontSize: 14.0, color: Colors.black);
+
+TextStyle underlineText = const TextStyle(
+    fontSize: 12.0, color: Colors.black, decoration: TextDecoration.underline);
+
+TextStyle commentText = const TextStyle(
+    fontSize: 14.0, color: Colors.grey, fontStyle: FontStyle.italic);
